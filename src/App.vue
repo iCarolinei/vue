409ba9f7dc54475e584170375cf18e62c9ca08b8
Caroline <caroline-jan@outlook.com>
2021-03-09 17:08:21 +0100

    fontawesome

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,13 @@
   <div id="app ">
     <div id="navbar">
      <ul class="nav nav-pills">
+     
   <li class="nav-item">
-    <a class="nav-link" href="#"><router-link to="/">Hello</router-link></a>
+        <a class="nav-link" href="#"><router-link to="/">Home</router-link></a>
   
         </li>
          <li class="nav-item dropdown">
-    <router-link to="/test"><a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">est </a></router-link>
+    <router-link to="/test"><a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Catégories</a></router-link>
     <div class="dropdown-menu">
       <a class="dropdown-item" href="#">Action</a>
       <a class="dropdown-item" href="#">Another action</a>
@@ -16,8 +17,44 @@
       <a class="dropdown-item" href="#">Separated link</a>
     </div>
   </li>
+<li class="nav-item dropdown">
+    <router-link to="/test"><a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Consoles</a></router-link>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Action</a>
+      <a class="dropdown-item" href="#">Another action</a>
+      <a class="dropdown-item" href="#">Something else here</a>
+      <div class="dropdown-divider"></div>
+      <a class="dropdown-item" href="#">Separated link</a>
+    </div>
+  </li>
+     <li class="nav-item dropdown">
+    <router-link to="/test"><a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Nouveautés</a></router-link>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Action</a>
+      <a class="dropdown-item" href="#">Another action</a>
+      <a class="dropdown-item" href="#">Something else here</a>
+      <div class="dropdown-divider"></div>
+      <a class="dropdown-item" href="#">Separated link</a>
+    </div>
+  </li>
+
         <li class="nav-item">
-    <a class="nav-link" href="#"><router-link to="/test">Test</router-link></a>
+    <a class="nav-link" href="#"><router-link to="/test"><font-awesome-icon icon="shopping-cart" /> Panier</router-link></a>
+  </li>
+   <li class="nav-item">
+    <a class="nav-link" href="#"><router-link to="/test"><font-awesome-icon icon="map-marker-alt" /> Magasin</router-link></a>
+  </li>
+ 
+ 
+        <li class="nav-item">
+    <a class="nav-link" href="#"><router-link to="/test"><font-awesome-icon icon="user-alt" /> Connexion</router-link></a>
+  </li>
+    <li class="nav-item dropdown">
+    <router-link to="/test"><a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Fr</a></router-link>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">En</a>
+    
+    </div>
   </li>
       
 </ul>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,22 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-
-
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+
+import { faUserAlt } from '@fortawesome/free-solid-svg-icons'
+
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faShoppingCart)
+library.add(faMapMarkerAlt )
+
+library.add(faUserAlt)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
