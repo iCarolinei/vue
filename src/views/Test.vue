a056fc0325780c3a3c4cab38c448c8510cb06383
Caroline <caroline-jan@outlook.com>
2021-03-09 11:38:15 +0100

    config

---
+++ src/views/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Test msg="Yo" />
+  </div>
+</template>
+
+<script>
+import Test from "../components/Test.vue";
+
+export default {
+  name: "Test",
+  components: {
+    Test
+  }
+};
+</script>
