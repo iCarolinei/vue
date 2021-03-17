f952c1ad8c244c65d9daa01d3fd8f6da5f0f7ee6
iCarolinei <caroline-jan@outlook.com>
2021-03-18 00:56:39 +0100

    map fix

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,38 +1,59 @@
 <template>
-  <!--<GoogleMapLoader
-    :mapConfig="mapConfig"
-    apiKey="AIzaSyAhwbLgAUIkAKGLcr_OjYmz5DBxrFhX3Is"
-  />-->
-
-  <GoogleMapLoader
-    :mapConfig="mapConfig"
-    apiKey="AIzaSyAhwbLgAUIkAKGLcr_OjYmz5DBxrFhX3Is"
-  >
-    <template slot-scope="{ google, map }">
-      {{ map }}
-      {{ google }}
-    </template>
-  </GoogleMapLoader>
+  <div id="container">
+    <div id="mapContainer"></div>
+  </div>
 </template>
 
-
-
 <script>
-import GoogleMapLoader from "./GoogleMapLoader";
-import { mapSettings } from "@/constants/mapSettings";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import icon from "../assets/marker.png";
 
 export default {
-  components: {
-    GoogleMapLoader,
+  name: "Map",
+  data() {
+    return {
+      center: [50.6333, 5.56667],
+    };
   },
+  methods: {
+    setupLeafletMap: function () {
+      const mapDiv = L.map("mapContainer").setView(this.center, 13);
+      L.tileLayer(
+        "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+        {
+          attribution:
+            'Map data (c) <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+          maxZoom: 18,
+          id: "mapbox/streets-v11",
+          accessToken:
+            "pk.eyJ1Ijoic3Bvb2tuaWNrIiwiYSI6ImNrbWUxdTk1cjJidTQyb2xhNmJraWZlOWUifQ.h_MnGGQVLTVyIHW02sCzTA",
+        }
+      ).addTo(mapDiv);
 
-  computed: {
-    mapConfig() {
-      return {
-        ...mapSettings,
-        center: { lat: 0, lng: 0 },
-      };
+      var markerIcon = L.icon({
+        iconUrl: icon,
+        iconSize: [38, 38],
+        iconAnchor: [22, 94],
+        popupAnchor: [-3, -76],
+      });
+
+      let marker = L.marker([50.6333, 5.56667], { icon: markerIcon }).addTo(
+        mapDiv
+      );
+      var popup = marker.bindPopup("<b>Notre magasin <3</b>");
+      popup.openPopup();
     },
+  },
+  mounted() {
+    this.setupLeafletMap();
   },
 };
 </script>
+
+<style scoped>
+#mapContainer {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
--- src/views/Store.vue
+++ src/views/Store.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="mapstore">
-    <TravelMap class="travel-map" />
+    <h1 class="d-flex justify-content-center pt-4 pb-4 form-title">
+      Magasin ❤️
+    </h1>
+    <!--<TravelMap class="travel-map" />-->
+    <Map class="map" />
   </div>
 </template>
 
 <script>
 //import Map from "../components/GoogleMapLoader.vue";
-import TravelMap from "../components/TravelMap";
+// import TravelMap from "../components/TravelMap";
+import Map from "../components/Map";
 export default {
   name: "Store",
   components: {
-    TravelMap,
+    //TravelMap,
+    Map,
   },
 };
 </script>
