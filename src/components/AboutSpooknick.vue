4e7ecd9ae83f0d7b52d29fd3589e5a6ccf762d9d
iCarolinei <caroline-jan@outlook.com>
2021-03-18 17:08:33 +0100

    carrousel

--- src/components/AboutSpooknick.vue
+++ src/components/AboutSpooknick.vue
@@ -19,20 +19,20 @@
         indicators
         background="#ababab"
         img-width="1024"
-        img-height="480"
+        img-height="400"
         style="text-shadow: 1px 1px 2px #333"
         @sliding-start="onSlideStart"
         @sliding-end="onSlideEnd"
       >
         <b-carousel-slide
-          img-src="https://picsum.photos/1024/480?grayscale"
+          img-src="https://picsum.photos/1024/400?grayscale"
         ></b-carousel-slide>
 
-        <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+        <b-carousel-slide img-src="https://picsum.photos/1024/400">
         </b-carousel-slide>
 
         <b-carousel-slide
-          img-src="https://picsum.photos/1024/480/?image=58"
+          img-src="https://picsum.photos/1024/400/?image=58"
         ></b-carousel-slide>
 
         <b-carousel-slide>
@@ -41,7 +41,7 @@
               class="d-block img-fluid w-100"
               width="1024"
               height="480"
-              src="https://picsum.photos/1024/480/?image=55"
+              src="https://picsum.photos/1024/400/?image=55"
               alt="image slot"
             />
           </template>
